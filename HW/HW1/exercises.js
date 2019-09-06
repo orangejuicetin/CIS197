5d62c79e38f6b0c105926dd63db611a4f492fbc5
orangejuicetin <justinchoi2000@gmail.com>
2019-09-06 01:50:27 -0400

    another dumb error, fixed typeof operator

--- HW/HW1/exercises.js
+++ HW/HW1/exercises.js
@@ -31,7 +31,7 @@
 var sum = function (arr) {
   var sum = 0;
   for (var i = 0; i < arr.length; i++) {
-    if (typeof arr[i] === Number) {
+    if (typeof arr[i] === 'number') {
       sum += arr[i];
     } else {
       throw console.error('element is NaN');
