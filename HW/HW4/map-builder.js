1563c28c96aa36fdc28633a5f63571419e7f22ca
orangejuicetin <justinchoi2000@gmail.com>
2019-09-28 12:07:22 -0400

    finally debugged setupPalette T.T

--- HW/HW4/map-builder.js
+++ HW/HW4/map-builder.js
@@ -5,37 +5,34 @@
 var DEFAULT_WIDTH = 30;
 var DEFAULT_HEIGHT = 15;
 
-var MapBuilder = function ($container, params) {
+var MapBuilder = function($container, params) {
   MapBuilder.prototype.$elem = $container;
   if (params) {
     this.width = params.width;
     this.height = params.height;
   } else {
-    this.width = DEFAULT_WIDTH; ``
+    this.width = DEFAULT_WIDTH;
     this.height = DEFAULT_HEIGHT;
   }
 };
 
 // TODO: Implement MapBuilder.setupPalette()
-MapBuilder.prototype.setupPalette = function () {
+MapBuilder.prototype.setupPalette = function() {
   MapBuilder.prototype.$palette = this.$elem.find('.palette');
-  console.log(this.$palette)
-  this.$palette.click(function (event) {
-    console.log(event)
-    MapBuilder.prototype.$selected = event.toElement;
-    console.log(this.$selected);
-    this.swatchName = getSwatchName(event);
-    console.log(this.swatchName)
-    this.$selected.addClass("selected");
-    console.log($selected.attr('class'));
-  })
+  this.$palette.click(function(e) {
+    $('.selected').removeClass('selected');
+    this.$curr = e.target;
+    $(this.$curr).addClass('selected');
+    this.swatchName = getSwatchName(this.$curr);
+  });
 };
 
-var getSwatchName = function (event) {
-  return event.target.classList.value;
-}
-// TODO: Implement MapBuilder.setupMapCanvas
-MapBuilder.prototype.setupMapCanvas = function () {
-  console.log('working');
-}
+var getSwatchName = function(input) {
+  // takes class name of event target
+  return input.classList.value;
+};
 
+// TODO: Implement MapBuilder.setupMapCanvas
+MapBuilder.prototype.setupMapCanvas = function() {
+  MapBuilder.prototype.$map = this.$elem.find('.map');
+};
