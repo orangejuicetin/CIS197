cc370913f833dd526d5374a8f1fbd1ebfc792120
orangejuicetin <justinchoi2000@gmail.com>
2019-11-02 16:08:10 -0400

    renamed folders

--- HW/hw6/models/question.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var questionSchema = new mongoose.Schema({
-  questionText: { type: String },
-  author: { type: String },
-  answer: { type: String }
-});
-
-module.exports = mongoose.model('Question', questionSchema);
